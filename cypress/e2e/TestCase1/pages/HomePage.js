be89261dc6261cf7b7d8c8eacc1a6c29164274a9
Anwar Sadat <sadat.m44@yahoo.com>
2025-03-22 00:42:34 +0600

    homepage updated

--- cypress/e2e/TestCase1/pages/HomePage.js
+++ cypress/e2e/TestCase1/pages/HomePage.js
@@ -7,11 +7,11 @@ class HomePage {
   }
 
   clickProducts() {
-    // cy.get('a[href="/products"]').click();
+    cy.get('a[href="/products"]').click();
     // cy.get('.shop-menu > .nav > :nth-child(2) > a').click();
-    cy.contains("a", "Products", { timeout: 15000 })
-      .should("be.visible")
-      .click();
+    // cy.contains("a", "Products", { timeout: 15000 })
+    //   .should("be.visible")
+    //   .click();
   }
 }
 
